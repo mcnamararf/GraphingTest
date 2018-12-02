6cc1608a06e8a5a190dbb3bb70772ca9c4962934
mcnamararf <mcnamararf@vcu.edu>
2018-12-01 22:04:32 -0500

    Sine Wave
    Sine Wave with Points

--- app/src/main/java/com/example/mcnamararf/graphing_testone/MainActivity.java
+++ app/src/main/java/com/example/mcnamararf/graphing_testone/MainActivity.java
@@ -6,31 +6,32 @@ import android.os.Bundle;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
+import com.jjoe64.graphview.series.PointsGraphSeries;
 
 public class MainActivity extends AppCompatActivity {
 
-    private LineGraphSeries<DataPoint> series1;
-
+    private PointsGraphSeries<DataPoint> series1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-     double x,y;
+     double x, y;
         x=0;
 
-        GraphView graph1 = (GraphView) findViewById(R.id.graph);
-        series1 = new LineGraphSeries<>();
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        series1 = new PointsGraphSeries<>();
 
 
         int numDataPoints = 500;
         for(int i = 0; i<numDataPoints; i++){
-            x = x + 0.5;
+            x = x + 0.1;
             y = Math.sin(x);
             series1.appendData(new DataPoint(x,y), true, 100);
         }
-        graph1.addSeries(series1);
+
+        graph.addSeries(series1);
 
     }
 }
